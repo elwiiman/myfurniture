d21320eb1a259b0599e4163c0558b5512b7b5697
elwiiman <emmanuel.sanro@gmail.com>
2021-10-13 18:42:37 -0500

    installing theme decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,11 @@
 import { GlobalStyles } from '../src/config/global.styles';
+import { ThemeProvider } from 'styled-components';
+import { withThemes } from '@react-theming/storybook-addon';
+import { addDecorator } from '@storybook/react';
+import { lightTheme, darkTheme } from '../src/config/theme_config';
+
+// pass ThemeProvider and array of your themes to decorator
+addDecorator(withThemes(ThemeProvider, [lightTheme, darkTheme]));
 
 // Global decorator to apply the styles to all stories
 export const decorators = [
