f07c142c1bfb19a44172b8cbf65ddc0ff12e2d6b
elwiiman <emmanuel.sanro@gmail.com>
2021-10-13 18:06:02 -0500

    Adding Logo component

---
+++ src/components/Logo/Logo.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ComponentMeta, Story } from '@storybook/react';
+
+import Logo from './Logo';
+
+export default {
+    title: 'Logo',
+    component: Logo,
+} as ComponentMeta<typeof Logo>;
+
+const Template: Story = () => <Logo />;
+
+export const Default = Template.bind({});
+Default.args = {};
