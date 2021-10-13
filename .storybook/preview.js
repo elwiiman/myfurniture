9bbd1940d4097bcb20597586686f86f80200c2ef
elwiiman <emmanuel.sanro@gmail.com>
2021-10-13 18:05:06 -0500

    adding global styles to storybook preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,21 @@
+import { GlobalStyles } from '../src/config/global.styles';
+
+// Global decorator to apply the styles to all stories
+export const decorators = [
+    (Story) => (
+        <>
+            <GlobalStyles />
+            <Story />
+        </>
+    ),
+];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
-}
+};
